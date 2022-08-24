0d54dd46f72c6139b3e284ec161ceac4f6d6b528
vagrant <eng.saad.najm@gmail.com>
2022-08-24 05:49:21 +0000

    validation for stores and employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Store name?"
+
+store_name = gets.chomp
+
+@new_store = Store.create(
+  name: store_name
+)
+
+puts @new_store.errors.full_messages
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :store, :first_name, :last_name, :hourly_rate, presence: true
+  validates :hourly_rate, numericality: {
+    greater_than: 40, less_than: 200 }
 end
