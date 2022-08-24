161f81f2e7089714f9db6cf587289837104df985
vagrant <eng.saad.najm@gmail.com>
2022-08-24 05:34:57 +0000

    adding employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Saad", last_name: "Alkhaleeli", hourly_rate: 30)
+@store1.employees.create(first_name: "Ahmed", last_name: "Adel", hourly_rate: 20)
+@store1.employees.create(first_name: "Mo", last_name: "Qasim", hourly_rate: 10)
+
+@store2.employees.create(first_name: "Siraj", last_name: "Edomnton", hourly_rate: 15)
+@store2.employees.create(first_name: "Wafi", last_name: "Ninja", hourly_rate: 19)
+@store2.employees.create(first_name: "Rami", last_name: "Malik", hourly_rate: 55)
